30f5c2e6aaa1e42172540bf31e1ac0b6e1d32693
Sailesh <Sailash@192.168.1.39>
2025-03-06 18:08:51 +0530

    collect.!

--- StreamsJava/src/com/streams/GetMapValues.java
+++ StreamsJava/src/com/streams/GetMapValues.java
@@ -1,5 +1,6 @@
 package com.streams;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,12 +9,20 @@ import com.streams.Data.Employee;
 import com.streams.Data.EmployeeHardCodeData;
 
 public class GetMapValues {
-
+	// COMPLEX Streams
 	public static void main(String[] args) {
 		List<Employee> list = EmployeeHardCodeData.getEmployeeDetails();
 		Map<Integer, String> map = list.stream().collect(Collectors.toMap(emp -> emp.getId(), emp -> emp.getName()));
-
 		System.out.println(map);
+
+		list = EmployeeHardCodeData.getEmployeeDetails();
+		Map<Integer, Object> highestSalaryByDept = list.stream()
+				.collect(Collectors.groupingBy(Employee::getAge,
+						Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingLong(Employee::getSalary)),
+								emp -> emp.map(Employee::getSalary))));
+
+		System.out.println("\n\n" + highestSalaryByDept);
+
 	}
 
 }
